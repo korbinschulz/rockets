e0a5103710b9845d460f2c45dde0f865e107b1a4
korbinschulz <korbkris@gmail.com>
2022-08-17 00:44:25 -0500

    Added loading div to wait for data to be fetched before trying to render rockets

--- frontend/src/containers/ViewRockets/ViewRockets.js
+++ frontend/src/containers/ViewRockets/ViewRockets.js
@@ -1,25 +1,33 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./ViewRockets.css";
 
 function ViewRockets() {
-  const getData = () => {
+  const [rockets, setRockets] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
     const getUrl = "http://localhost:3000/api/rockets/";
     axios
       .get(getUrl)
       .then(function (res) {
         console.log("Data has been fetched!");
         console.log(res);
+        setRockets(res);
+        setIsLoading(false);
       })
       .catch(function (error) {
         console.log(error);
         alert("Error fetching rockets from database, please try again!");
       });
-  };
-
-  useEffect(() => {
-    getData();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="view-rockets">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
 
   return (
     <div className="view-rockets">
