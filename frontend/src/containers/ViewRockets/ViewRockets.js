d30a27639d1727776c1cf96fd6dbf103533d74ea
korbinschulz <korbkris@gmail.com>
2022-08-16 20:36:04 -0500

    Removed strict mode to prevent useEffect mounting twice

--- frontend/src/containers/ViewRockets/ViewRockets.js
+++ frontend/src/containers/ViewRockets/ViewRockets.js
@@ -1,10 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import "./ViewRockets.css";
 
 function ViewRockets() {
+  const getData = () => {
+    const getUrl = "http://localhost:3000/api/rockets/";
+    axios
+      .get(getUrl)
+      .then(function (res) {
+        console.log("Data has been fetched!");
+        console.log(res);
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("Error fetching rockets from database, please try again!");
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="view-rockets">
       <h2>
-        The Rocket(s) below are the ones currently existing in the database.
+        The Rockets below are the ones currently existing in the database.
       </h2>
     </div>
   );
